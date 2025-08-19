66d5b273a3a5aa24a9f4785c8d62e6b72db57e72
JustAnnicka <annicka@Medusa.local>
2025-08-19 18:11:31 +0200

    Added display for guessed letters and check if a letter had already been guessed so that the user does not loose extra lives

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ import random
 display = ""
 guess = ""
 correct_letters = []
+guessed_letters = "" #for displayin all the guessed letters (wrong ones only)
 
 language = input("please select a language:\nType 'en' for english\nType 'es' for spanish\nType 'de' for german\nType 'all' for a random word from any language\n").lower()
 #change display messages accorgdingly as well
@@ -13,12 +14,14 @@ elif language == "en":
     chosen_word = random.choice(assets.words_english)
 elif language == "de":
     chosen_word = random.choice(assets.words_german)
-elif language == "any":
+elif language == "all":
     chosen_word = random.choice(assets.word_list)
 else:
     print("That was not an option default to any")
     chosen_word = random.choice(assets.word_list)
 
+#add dificulty for game (hard will start on hangman 4 and and have only 6 lives, easy the standard 10)
+#Add word guesses (count only as one live)
 word_len = len(chosen_word)
 for position in range(word_len):
     display += "-"
@@ -39,8 +42,10 @@ def update_display(display):
 
 def display_game():
     i =  10 - assets.lives
-    print(f"{assets.hangman_game[i]}\n\n {display}")
-    
+    print(f"{assets.hangman_game[i]}\n\n {display}\n\n")
+    if len(guessed_letters) > 0:
+        print(f"not in word: {guessed_letters}")
+#add valid input function (checks for multiple characters, if input is a symbol or number) Can also check repeated guesses
 while assets.end_game == False:
     guess = input("Guess a letter: ").lower() # check this for multiple inputs , eg word and for numbers/symbols
     display =  update_display(display)
@@ -50,10 +55,29 @@ while assets.end_game == False:
             print("You win, great Job!")
             assets.end_game = True
     else:
-        assets.lives -= 1
+        if guess in guessed_letters:
+            print("You guessed that already")
+        else:
+            guessed_letters += guess + ", "
+            assets.lives -= 1
         if assets.lives <= 0:
             print("Game Over")
             assets.end_game = True
         else:
             print(f"wrong number of lives remaining {assets.lives}")
     display_game()
+
+#Necessary updates
+# - Add check for valid input/guess
+# - Change display messages to selected language
+
+#Future functionality 
+# - Add hard or easy mode (6 or 10 lives)
+# - Add word guesses (only valid if len of word) (cost one live)
+# - Integrate a dictionary/language library (no more manual created lists)
+# - For defined language have choice (easy and hard) wether user want so write accents/umlaut manually or use simple method
+# --> simple method would be different accoding to language
+# --> e.g Spanish easy will accept 'n' for 'ñ' and 'c' for 'ç' 
+# --> e.g in german you can select wether 'a' should count as 'ä' or we use the 'ae' for 'ä'(extending the word len) 
+# --> e.g if there are symbols in the word these will be shown (easy mode) eg "mba'eichapa" would be "___'_______"
+# - Check if we can update the terminal staticly eg clear and overwrite rather than diplay all messages below eachother
